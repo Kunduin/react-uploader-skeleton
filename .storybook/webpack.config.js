711c7f0e663374935004c52ea9896d70733e9330
kunduin <15852158126@163.com>
2019-05-11 10:00:19 +0800

    test(storybook): add storybook for ui test

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,27 @@
+const path = require('path');
+const SRC_PATH = path.join(__dirname, '../src');
+const STORIES_PATH = path.join(__dirname, '../stories');
+const CONFIG_PATH = path.join(__dirname, '../tsconfig.js');
 module.exports = ({ config, mode }) => {
+  if (Array.isArray(config.entry)) {
+    const { entry } = config;
+    entry.splice(entry.findIndex(e => /.*polyfills.js$/.test(e)), 1);
+  }
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve("babel-loader"),
-    options: {
-      presets: [["react-app", { flow: false, typescript: true }]]
-    }
+    include: [SRC_PATH, STORIES_PATH],
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+        options: {
+          configFileName: CONFIG_PATH
+        }
+      },
+      { loader: require.resolve('react-docgen-typescript-loader') }
+    ],
+    exclude: /node_modules/
   });
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.extensions.push('.ts', '.tsx');
+  delete config.resolve.alias['core-js'];
   return config;
 };
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,8 +1,17 @@
-import { storiesOf } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+import { action } from "@storybook/addon-actions";
+import { storiesOf, StoryDecorator } from "@storybook/react";
 import React from "react";
 
-import ReactUploaderSkeleton from "../src";
+import { ReactUploaderSkeleton } from "../src";
 
-storiesOf("Hello World", module).add("with text", () => (
-  <ReactUploaderSkeleton anyText="meme" />
-));
+const CenterDecorator: StoryDecorator = storyFn => (
+  <div style={{ margin: "40px auto", maxWidth: 800 }}>{storyFn()}</div>
+);
+
+storiesOf("Hello World", module)
+  .addDecorator(withA11y)
+  .addDecorator(CenterDecorator)
+  .add("with text", () => (
+    <ReactUploaderSkeleton onClick={action("my-click")} anyText="meme" />
+  ));
